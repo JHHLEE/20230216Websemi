92f27107b3e57c7a0c64098d72d9b420463b1a79
Jeonghyeon Lee <ctrlad321@gmail.com>
2023-02-20 00:41:56 +0900

    Create CommentDao.java

---
+++ src/board/model/dto/CommentDao.java
@@ -0,0 +1,17 @@
+package board.model.dto;
+
+import java.sql.Connection;
+import java.util.List;
+
+import board.model.vo.CommentVo;
+
+public class CommentDao {
+
+	public List<CommentVo> getCommentList(Connection conn){
+		List<CommentVo>result = null;
+		
+		
+		return result;
+	}
+	
+}
